ac5c074f58a39d2414bf26c8247c3da982354188
Elizabeth Harper <foxcapade@gmail.com>
2020-05-13 11:56:31 -0400

    added mkdir for main/resources

--- bin/generate-docs.sh
+++ bin/generate-docs.sh
@@ -6,6 +6,6 @@ set -eu
 
 echo "${C_BLUE}Generating API Documentation${C_NONE}"
 
-mkdir -p "docs"
+mkdir -p "docs" "src/main/resources"
 raml2html .tools/raml/full-api.raml > docs/api.html
 cp docs/api.html src/main/resources/api.html
