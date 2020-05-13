cbc91a5a41fa3147e2d86aeed6373cafcbb8979c
Elizabeth Harper <foxcapade@gmail.com>
2020-05-13 00:11:43 -0400

    fixed paths

--- bin/install-raml2jaxrs.sh
+++ bin/install-raml2jaxrs.sh
@@ -4,7 +4,7 @@
 
 echo "${C_BLUE}Checking for Raml to JaxRS${C_NONE}"
 
-if ! stat ./tools/bin/raml-to-jaxrs.jar > /dev/null 2>&1; then
+if ! stat .tools/bin/raml-to-jaxrs.jar > /dev/null 2>&1; then
   echo "${C_CYAN}  Not found.  Installing.${C_NONE}"
   .tools/bin/build-raml2jaxrs.sh 2>&1 | sed 's/^/  /'
 fi
