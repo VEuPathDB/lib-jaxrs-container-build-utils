a58a3e8409769da0999cf6eb2dc93d2695f5f173
Elizabeth Harper <foxcapade@gmail.com>
2020-06-17 13:28:43 -0400

    updated to reflect RAML move to new repo

--- bin/generate-jaxrs.sh
+++ bin/generate-jaxrs.sh
@@ -5,7 +5,7 @@
 APP_PACKAGE=${1:?Missing required application package param}
 
 compile() {
-  java -jar .tools/bin/raml-to-jaxrs.jar .tools/raml/full-api.raml \
+  java -jar .tools/bin/raml-to-jaxrs.jar api.raml \
     --directory src/main/java \
     --generate-types-with jackson \
     --model-package "${APP_PACKAGE}".generated.model \
